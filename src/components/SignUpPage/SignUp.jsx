7c356d0e18c74cb69b9880b6991cb2cb51fd0414
GregWasz <gregory.waszkow@icloud.com>
2021-11-09 11:53:29 +0100

    Fullfiled SignUp Page

--- src/components/SignUpPage/SignUp.jsx
+++ src/components/SignUpPage/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Avatar from "@mui/material/Avatar";
 import { Typography } from "@mui/material";
@@ -9,8 +9,38 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
+import axios from "../../api/axios";
+import { useHistory } from "react-router";
+import ErrorAlert from "../CommonComponents/ErrorAlert";
 
 export default function SignUp() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const history = useHistory();
+
+    async function handleSignUp(e) {
+        e.preventDefault();
+        setIsError(false);
+
+        await axios
+            .post("/users", { username, email, password })
+            .then(() => {
+                history.push("/");
+            })
+            .catch((error) => {
+                setIsError(true);
+                setErrorMessage();
+                try {
+                    setErrorMessage(error.response.data.detail.Error);
+                } catch {
+                    setErrorMessage("Something went wrong");
+                }
+            });
+    }
+
     return (
         <>
             <Container component="main" maxWidth="xs">
@@ -34,6 +64,7 @@ export default function SignUp() {
                         // onSubmit={handleSubmit}
                         sx={{ mt: 3 }}
                     >
+                        {isError && <ErrorAlert message={errorMessage} />}
                         <Grid container spacing={2}>
                             <Grid item xs={12}>
                                 <TextField
@@ -44,6 +75,9 @@ export default function SignUp() {
                                     id="username"
                                     label="Username"
                                     autoFocus
+                                    onChange={(e) =>
+                                        setUsername(e.target.value)
+                                    }
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -54,6 +88,7 @@ export default function SignUp() {
                                     label="Email Address"
                                     name="email"
                                     autoComplete="email"
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -64,7 +99,10 @@ export default function SignUp() {
                                     label="Password"
                                     type="password"
                                     id="password"
-                                    autoComplete="new-password"
+                                    autoComplete="password"
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
                                 />
                             </Grid>
                         </Grid>
@@ -73,7 +111,7 @@ export default function SignUp() {
                             fullWidth
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
-                            href="/"
+                            onClick={handleSignUp}
                         >
                             Sign Up
                         </Button>
