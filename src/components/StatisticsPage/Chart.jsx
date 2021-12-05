2670203b7f0b5eee7ea75c55c118f2b433bd347d
GregWasz <gregory.waszkow@icloud.com>
2021-12-06 00:39:56 +0100

    Added Chart Control

--- src/components/StatisticsPage/Chart.jsx
+++ src/components/StatisticsPage/Chart.jsx
@@ -1,5 +1,5 @@
 import { Grid, Paper } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import {
     LineChart,
     Line,
@@ -10,6 +10,13 @@ import {
 } from "recharts";
 
 export default function Chart(props) {
+    var chartData = props.bodyWeights.map((ele) => {
+        return {
+            weight_amount: ele.weight_amount,
+            weighting_date: ele.weighting_date.replace("T", " ").slice(0, -16),
+        };
+    });
+
     return (
         <Grid item xs={12} md={12} lg={12}>
             <Paper
@@ -19,8 +26,8 @@ export default function Chart(props) {
                     justifyContent: "center",
                 }}
             >
-                <LineChart width={500} height={300} data={props.bodyWeights}>
-                    <XAxis dataKey="weighting_date" tick={false} />
+                <LineChart width={500} height={300} data={chartData}>
+                    <XAxis dataKey="weighting_date" />
                     <YAxis dataKey="weight_amount" />
                     <CartesianGrid stroke="#ccc" />
                     <Tooltip />
