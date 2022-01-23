85070547cfcdbec06df3b9eab38551528f376d1d
GregWasz <gregory.waszkow@icloud.com>
2022-01-23 13:39:11 +0100

    Pagination

--- src/components/ForumPage/Forum.jsx
+++ src/components/ForumPage/Forum.jsx
@@ -1,15 +1,20 @@
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, Paper } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { Box } from "@mui/system";
 import React, { useState, useEffect } from "react";
 import PostItem from "./PostItem";
 import CreatePostForm from "./CreatePostForm";
 import axios from "../../api/axios";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 export default function Forum() {
     /* Wyświetlanie strony forum */
+    const paginationValue = 5;
     const [currentUserID, setCurrentUserID] = useState("");
     const [posts, setPosts] = useState([]);
+    const [postsPage, setPostsPage] = useState([]);
+    const [page, setPage] = useState(0);
 
     async function getUser() {
         /* Odwołanie bezpośrednie do aplikacji serwerowej w celu zautoryzowania użytkownika */
@@ -26,8 +31,22 @@ export default function Forum() {
             .get("/posts")
             .then((response) => {
                 setPosts(response.data);
+                setPostsPage(response.data.slice(0, paginationValue));
+                setPage(1);
             })
             .catch((error) => {});
+    }
+
+    function handlePagination(value) {
+        /* Stronicowanie po zmianie strony */
+        setPage(value);
+        value -= 1;
+        setPostsPage(
+            posts.slice(
+                value * paginationValue,
+                value * paginationValue + paginationValue,
+            ),
+        );
     }
 
     useEffect(() => {
@@ -40,7 +59,7 @@ export default function Forum() {
             <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
                 <Grid container spacing={3}>
                     <CreatePostForm getPosts={getPosts} />
-                    {posts.map((post) => (
+                    {postsPage.map((post) => (
                         <PostItem
                             key={post.post_id}
                             post={post}
@@ -49,6 +68,24 @@ export default function Forum() {
                         />
                     ))}
                 </Grid>
+                <Paper
+                    sx={{
+                        mt: 3,
+                        p: 1,
+                        display: "flex",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Stack spacing={2}>
+                        <Pagination
+                            count={Math.ceil(posts.length / paginationValue)}
+                            page={page}
+                            onChange={(e, value) => {
+                                handlePagination(value);
+                            }}
+                        />
+                    </Stack>
+                </Paper>
             </Container>
         </Box>
     );
